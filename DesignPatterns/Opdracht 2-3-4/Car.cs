5e66d017a43bacbaebd53db1b96b4512238ec107
Dion van 't Veer <dionnv@gmail.com>
2015-02-21 16:42:48 +0100

    Kleine toevoeging

--- DesignPatterns/Opdracht 2-3-4/Car.cs
+++ DesignPatterns/Opdracht 2-3-4/Car.cs
@@ -1,57 +1,46 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
-namespace Design Patterns.Opdracht2
+namespace DesignPatterns.Opdracht2_3_4
 {
 	public class Car
 	{
-		public Engine Engine
-		{
-			get
-			{
-				throw new NotImplementedException();
-			}
-			set
-			{
-				throw new NotImplementedException();
-			}
-		}
+        public Engine Engine
+        {
+            get;
+            set;
+        }
 
-		public string Brand
-		{
-			get
-			{
-				throw new NotImplementedException();
-			}
-			set
-			{
-				throw new NotImplementedException();
-			}
-		}
+        public string Brand
+        {
+            get;
+            set;
+        }
 
-		public string Type
-		{
-			get
-			{
-				throw new NotImplementedException();
-			}
-			set
-			{
-				throw new NotImplementedException();
-			}
-		}
+        public string Type
+        {
+            get;
+            set;
+        }
 
-		public FuelTank FuelTank
-		{
-			get
-			{
-				throw new NotImplementedException();
-			}
-			set
-			{
-				throw new NotImplementedException();
-			}
-		}
+        public FuelTank FuelTank
+        {
+            get;
+            set;
+        }
+
+        public void Drive()
+        {
+            Engine.Start();
+            Engine.Revs = 1000;
+            while(FuelTank.Content > 0)
+            {
+                FuelTank.Content -= Engine.FuelUsagePerSecond;
+                Thread.Sleep(999);
+            }
+            Engine.Stop();
+        }
 	}
 }
